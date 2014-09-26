64685615f00d93305172ebe2c3b4b8f1ad1a4af8
Filipe Bezerra <filipebzerra@gmail.com>
2014-09-26 18:02:28 -0300

    Funções principais adicionadas

---
+++ src/js/StoreApp.js
@@ -0,0 +1,98 @@
+(function() {
+	var data = [
+		{
+			name : 'Bull Terrier', 
+			price : 122.5,
+			model : 'Kolly 879-GRAFITE',
+			available : false,
+            quantity: 0,
+            image : "img/Kolly 879-GRAFITE.jpg",
+            reviews : [
+                {
+                    author: "grace@bol.com",
+                    message: "Until now, I have good experience with it",
+                    stars : 5
+                }
+            ]
+		},
+		{
+			name : 'Bull Terrier', 
+			price : 182.90,
+			model : 'Fanny 1230-CONCRETO',
+			available : true,
+            quantity: 100,
+            image : "img/Fanny 1230-CONCRETO.jpg",
+            reviews : [
+                {
+                    author: "brb@gmail.com",
+                    message: "Don't be so evil",
+                    stars : 1
+                }
+            ]
+		},
+		{
+			name : 'Bull Terrier', 
+			price : 85.5,
+			model : 'Fanny 1232-Mustang',
+			available : true,
+            quantity: 1,
+            image : "img/Fanny 1232-Mustang.jpg",
+            reviews : [
+                {
+                    author: "lana.ferris@gmail.com",
+                    message: "Beautiful",
+                    stars : 4
+                }
+            ]
+		},
+		{
+			name : 'Bull Terrier', 
+			price : 99.78,
+			model : 'Tagg Low 2-Preto',
+			available : true,
+            quantity: 59,
+            image : "img/Tagg Low 2-Preto.jpg",
+            reviews : [
+                {
+                    author: "joster@gmail.com",
+                    message: "The choosen one",
+                    stars : 5
+                }
+            ]
+		}
+	];
+
+    var storeApp = angular.module('storeApp', []);
+
+	storeApp.controller("StoreController", ['$scope', function($scope) {
+		this.products = data;
+
+        $scope.numLimit = data.length;
+        $scope.amount = 0.0;
+        $scope.count = 0;
+
+        this.buy = function(item) {
+            $scope.amount += item.price;
+            $scope.count++;
+
+            item.quantity--;
+
+            if (item.quantity == 0) {
+                item.available = false;
+            };
+        };
+	}]);
+
+    storeApp.controller("PanelController", ['$scope', function($scope) {
+        this.panel = 1;
+
+        this.selectPanel = function(setPanel) {
+            this.panel = setPanel;
+        };
+
+        this.isSelected = function(checkPanel) {
+            return this.panel === checkPanel;
+        };
+    }]);
+
+})();
